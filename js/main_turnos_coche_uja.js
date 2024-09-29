631850dd8ddbab3c6c682c642ab00ba1b999e831
Victor M. Rivas <vrivas@ujaen.es>
2024-09-29 08:42:42 +0200

    Mostrar semana funcionando

--- js/main_turnos_coche_uja.js
+++ js/main_turnos_coche_uja.js
@@ -17,7 +17,13 @@ function mostrarDiaHoy(){
     let hoy=new Date();
     mostrarDias( "Hoy, "+hoy.toDD_MMM_YYYY(), D.filter(d => d.fecha.toComparableString()==hoy.toComparableString()) );    
 }
-
+function mostrarDiasSemana(){
+    let hoy=new Date();
+    let diaSemana=hoy.getDay();
+    diaSemana=diaSemana==0?7:diaSemana;
+    let posicion=D.findIndex(d => d.fecha.toComparableString()==hoy.toComparableString());
+    mostrarDias( "Semana, "+hoy.toDD_MMM_YYYY(), D.slice(posicion-diaSemana+1,posicion-diaSemana+8) );    
+}
 
 // Por defecto, mostramos los días del cuatrimestre
 mostrarDiasCuatrimestre();
