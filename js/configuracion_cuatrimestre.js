1e4e5e4705a3386523a3c8f35ba13eca804a8668
Victor M. Rivas <vrivas@ujaen.es>
2025-01-21 18:43:30 +0100

    Actualizar configuración del 2º cuatrimestre, incluyendo nuevos festivos y fechas de inicio y fin.

--- js/configuracion_cuatrimestre.js
+++ js/configuracion_cuatrimestre.js
@@ -1,17 +1,33 @@
 let cuatrimestre = {
-    "numero": 1,
-    "curso": "2024-2025",
-    "inicio": fecha(16, 9, 2024),
-    "fin": fecha(20, 12, 2024),
-    "festivos": 
+  numero: 2,
+  curso: "2024-2025",
+  inicio: fecha(27, 1, 2025),
+  fin: fecha(16, 5, 2025),
+  /*"festivos": 
     [ { "nombre": "Navidad", "inicio": fecha(21, 12, 2024), "fin": fecha(6, 1, 2025)}, 
         {"nombre": "El Pilar","inicio": fecha(12, 10, 2024)},
         {"nombre": "Festividad Jaén","inicio": fecha(18, 10, 2024)},
         {"nombre": "Todos los Santos","inicio": fecha(1, 11, 2024)},
         {"nombre": "Día de la Constitución","inicio": fecha(6, 12, 2024)},
         {"nombre": "Día de la Inmaculada","inicio": fecha(9, 12, 2024)}
-    ], // Festivos
-    getTitulo: function() {
-        return this.numero+"<sup>"+(this.numero==1?"er":"o")+"</sup> Cuatrimestre, "+this.curso;
-    }
-}
+    ], // Festivos primer cuatrim*/
+  festivos: [
+    {
+      nombre: "Semana Santa",
+      inicio: fecha(14, 4, 2025),
+      fin: fecha(20, 4, 2025),
+    },
+    { nombre: "Día de Andalucía", inicio: fecha(28, 2, 2025) },
+    { nombre: "Día del (NO) Trabajo", inicio: fecha(1, 5, 2024) },
+  ], // Festivos segundo cuatrim*/
+
+  getTitulo: function () {
+    return (
+      this.numero +
+      "<sup>" +
+      (this.numero == 1 ? "er" : "o") +
+      "</sup> Cuatrimestre, " +
+      this.curso
+    );
+  },
+};
