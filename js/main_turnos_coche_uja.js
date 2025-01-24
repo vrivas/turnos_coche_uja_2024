c3985bef1b403f775a0c53a3d6cd5f177ae5c0d9
Victor M. Rivas <vrivas@ujaen.es>
2025-01-24 17:14:15 +0100

    Mensajes de turnos no encontrados cuando las fechas indicadas no pertenecen al cuatrim

--- js/main_turnos_coche_uja.js
+++ js/main_turnos_coche_uja.js
@@ -1,30 +1,42 @@
-function mostrarDias(titulo, dias, clases=[]){
-    let divTitulo=document.getElementById("titulo");
-    let divContenido=document.getElementById("contenido");
-    divTitulo.innerHTML=titulo
-    let msj="";
-    dias.forEach( d => {
-        msj+=d.toDiv(clases)+"\n";
-    });
-    divContenido.innerHTML=msj;
-    
+function mostrarDias(titulo, dias = [], clases = []) {
+  let divTitulo = document.getElementById("titulo");
+  let divContenido = document.getElementById("contenido");
+  divTitulo.innerHTML = titulo;
+  let msj = "";
+  dias.forEach((d) => {
+    msj += d.toDiv(clases) + "\n";
+  });
+
+  msj =
+    msj == ""
+      ? "No se ha encontrado ningún turno operativo en este periodo"
+      : msj;
+  divContenido.innerHTML = msj;
 }
-function mostrarDiasCuatrimestre(){
-    mostrarDias(cuatrimestre.getTitulo(),D);    
+function mostrarDiasCuatrimestre() {
+  mostrarDias(cuatrimestre.getTitulo(), D);
 }
 
-function mostrarDiaHoy(){
-    let hoy=new Date();
-    mostrarDias( hoy.toDW_DD_MMM_YYYY(), 
-                D.filter(d => d.fecha.toComparableString()==hoy.toComparableString()),
-                ["dia-pantalla-completa"]  );    
+function mostrarDiaHoy() {
+  let hoy = new Date();
+
+  mostrarDias(
+    hoy.toDW_DD_MMM_YYYY(),
+    D.filter((d) => d.fecha.toComparableString() == hoy.toComparableString()),
+    ["dia-pantalla-completa"]
+  );
 }
-function mostrarDiasSemana(){
-    let hoy=new Date();
-    let diaSemana=hoy.getDay();
-    diaSemana=diaSemana==0?7:diaSemana;
-    let posicion=D.findIndex(d => d.fecha.toComparableString()==hoy.toComparableString());
-    mostrarDias( "Semana, "+hoy.toDD_MMM_YYYY(), D.slice(posicion-diaSemana+1,posicion-diaSemana+8) );    
+function mostrarDiasSemana() {
+  let hoy = new Date();
+  let diaSemana = hoy.getDay();
+  diaSemana = diaSemana == 0 ? 7 : diaSemana;
+  let posicion = D.findIndex(
+    (d) => d.fecha.toComparableString() == hoy.toComparableString()
+  );
+  mostrarDias(
+    "Semana, " + hoy.toDD_MMM_YYYY(),
+    D.slice(posicion - diaSemana + 1, posicion - diaSemana + 8)
+  );
 }
 
 // Por defecto, mostramos los días del cuatrimestre
