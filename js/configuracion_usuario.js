266463eb99639466acfa8c0ae504cdf37103940f
Victor M. Rivas <vrivas@ujaen.es>
2025-01-26 07:38:25 +0100

    Añade archivo de configuración para la vista de usuario

---
+++ js/configuracion_usuario.js
@@ -0,0 +1,13 @@
+/**
+ * @file configuracion_usuario.js
+ * @description Archivo de configuración de la vista de configuración de usuario.
+ * @requires js/cookies.js
+ * @author Víctor Rivas <vrivas@ujaen.es>
+ * @date 26-ene-2025
+ */
+
+let USER_CONFIG = {
+  nombre: null,
+  correo: null,
+  vistaPorDefecto: null,
+};
