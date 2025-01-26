9358b1b5055943f4a2aea852a9615de561b617c1
Victor M. Rivas <vrivas@ujaen.es>
2025-01-26 07:34:51 +0100

    Añade funciones para las cookies

---
+++ js/cookies.js
@@ -0,0 +1,33 @@
+/**
+ * Establece una cookie
+ * @param {string} cname Nombre de la cookie
+ * @param {string} cvalue Valor de la cookie
+ * @param {int} exdays Días por los que se mantiene la cookie
+ */
+function setCookie(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+  let expires = "expires=" + d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+/**
+ * Obtiene el valor de una cookie
+ * @param {string} cname Nombre de la cookie
+ * @returns {string} Valor de la cookie
+ */
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(";");
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == " ") {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
