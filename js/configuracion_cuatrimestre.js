059fc62e6ac213d41ba13fe1e0943b8ae4921c70
Victor M. Rivas <vrivas@ujaen.es>
2025-09-07 17:50:31 +0200

    Configuro 1er cuat 25-26

--- js/configuracion_cuatrimestre.js
+++ js/configuracion_cuatrimestre.js
@@ -1,25 +1,36 @@
 let cuatrimestre = {
-  numero: 2,
-  curso: "2024-2025",
-  inicio: fecha(27, 1, 2025),
-  fin: fecha(16, 5, 2025),
-  /*"festivos": 
-    [ { "nombre": "Navidad", "inicio": fecha(21, 12, 2024), "fin": fecha(6, 1, 2025)}, 
-        {"nombre": "El Pilar","inicio": fecha(12, 10, 2024)},
-        {"nombre": "Festividad Jaén","inicio": fecha(18, 10, 2024)},
-        {"nombre": "Todos los Santos","inicio": fecha(1, 11, 2024)},
-        {"nombre": "Día de la Constitución","inicio": fecha(6, 12, 2024)},
-        {"nombre": "Día de la Inmaculada","inicio": fecha(9, 12, 2024)}
-    ], // Festivos primer cuatrim*/
+  numero: 1,
+  curso: 2025 + "-" + 2026,
+  inicio: fecha(8, 9, 2025), // fecha( 2,2,2026),
+  fin: fecha(19, 12, 2025), // fecha(22, 5, 2026),
   festivos: [
     {
+      nombre: "Navidad",
+      inicio: fecha(20, 12, 2025),
+      fin: fecha(6, 1, 2026),
+    },
+    { nombre: "El Pilar", inicio: fecha(12 + 1, 10, 2025) },
+    { nombre: "Festividad Jaén", inicio: fecha(18, 10, 2025) },
+    { nombre: "Todos los Santos", inicio: fecha(1, 11, 2025) },
+    {
+      nombre: "Día de la Constitución",
+      inicio: fecha(6, 12, 2025),
+    },
+    {
+      nombre: "Día de la Inmaculada",
+      inicio: fecha(8 + 1, 12, 2025),
+    },
+    { nombre: "Inauguración curso", inicio: fecha(18, 9, 2025) },
+  ], // Festivos primer cuatrim
+  /*festivos: [
+    {
       nombre: "Semana Santa",
-      inicio: fecha(14, 4, 2025),
-      fin: fecha(20, 4, 2025),
+      inicio: fecha(28, 3, 2026),
+      fin: fecha(5, 4, 2026),
     },
     { nombre: "Día de Andalucía", inicio: fecha(28, 2, 2025) },
     { nombre: "Día del (NO) Trabajo", inicio: fecha(1, 5, 2025) },
-  ], // Festivos segundo cuatrim*/
+  ], // Festivos segundo cuatrim */
 
   getTitulo: function () {
     return (
